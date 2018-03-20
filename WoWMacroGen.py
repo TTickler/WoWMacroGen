a87187793eaa5e96f6b6aa4308e646d4af615023
clannadqs <cdozier3@students.kennesaw.edu>
2018-03-19 23:47:53 -0400

    formatted code to PEP-8 standard

--- WoWMacroGen.py
+++ WoWMacroGen.py
@@ -2,94 +2,82 @@ import ConfigParser
 import os
 
 
-
 class Macro(object):
-	def __init__(self):
-		self.test = 5
-		self._target = ''
-		self._spell = ''
-		self._icon = ''
-		self._command = ''
+    def __init__(self):
+        self.test = 5
+        self._target = ''
+        self._spell = ''
+        self._icon = ''
+        self._command = ''
 
-	
-	#property for the target of macro being generated	
-	@property
-	def target(self):
-		return self._target
+    # property for the target of macro being generated
+    @property
+    def target(self):
+        return self._target
 
-	#setter for target property
-	@target.setter
-	def target(self, target):
-		self._target = target
+    # setter for target property
+    @target.setter
+    def target(self, target):
+        self._target = target
 
-	@property 
-	def spell(self):
-		return self._spell
+    @property
+    def spell(self):
+        return self._spell
 
-	@spell.setter
-	def spell(self, spell):
-		self._spell = spell
+    @spell.setter
+    def spell(self, spell):
+        self._spell = spell
 
-	@property
-	def icon(self):
-		return self._icon
+    @property
+    def icon(self):
+        return self._icon
 
-	@icon.setter
-	def icon(self, icon):
-		self._icon = icon
+    @icon.setter
+    def icon(self, icon):
+        self._icon = icon
 
-	@property
-	def command(self):
-		return self._command
+    @property
+    def command(self):
+        return self._command
 
-	@command.setter
-	def command(self, command):
-		self._command = command
+    @command.setter
+    def command(self, command):
+        self._command = command
+
 
 class MacroCLI(object):
-	def __init__(self):
-		self.test = 5
-	
+    def __init__(self):
+        self.test = 5
 
+    # returns True if input is valid syntax from CLI
+    # else, returns false
+    def checkSyntax(self, args, Macro):
 
-	#returns True if input is valid syntax from CLI
-	#else, returns false
-	def checkSyntax(self, args, Macro):
+        # list of valid flags allowed in CLI. Allows future additions without hassle
+        validFlags = ['target', 'icon', 'spell', 'character']
 
-		#list of valid flags allowed in CLI. Allows future additions without hassle
-		validFlags = ['target','icon','spell', 'character']
+        for arg in args:
+            if arg not validArgument(arg):
+                return false
+            else:
 
+        return True
 
-		for arg in args:
-			if arg not validArgument(arg):
-				return false
-			else:
-				
-		
+        return False
 
-		return True
+    # returns true if specific argument contains valid flags and input
+    # else, returns false
+    def validArgument(self, argument):
 
-		return False
+        return True
 
-	#returns true if specific argument contains valid flags and input
-	#else, returns false
-	def validArgument(self, argument):
-		
-
-		return True
-
-		return False
-
+        return False
 
 
 if __name__ == "__main__":
-	from sys import argv
+    from sys import argv
 
-	myargs = getopts(argv)
+    myargs = getopts(argv)
 
-	macro = Macro()
-	macroCLI = MacroCLI()
-		
-
-
-
+    macro = Macro()
+    macroCLI = MacroCLI()
