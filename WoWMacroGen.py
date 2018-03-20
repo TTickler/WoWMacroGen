0031e0fcc56376b075c352b24b55a5f3c5f314e7
clannadqs <cdozier3@students.kennesaw.edu>
2018-03-19 23:32:47 -0400

    WoWMacroGen.py fleshed out. While loop in __main__ removed as this will act as a single command in the CLI. If it fails a valid warning/error will be thrown, but no loop will occur.

--- WoWMacroGen.py
+++ WoWMacroGen.py
@@ -41,15 +41,46 @@ class Macro(object):
 class MacroCLI(object):
 	def __init__(self):
 		self.test = 5
+	
 
+
+	#returns True if input is valid syntax from CLI
+	#else, returns false
+	def checkSyntax(self, args, Macro):
+
+		#list of valid flags allowed in CLI. Allows future additions without hassle
+		validFlags = ['target','icon','spell', 'character']
+
+
+		for arg in args:
+			if arg not validArgument(arg):
+				return false
+			else:
+				
+		
+
+		return True
+
+		return False
+
+	#returns true if specific argument contains valid flags and input
+	#else, returns false
+	def validArgument(self, argument):
+		
+
+		return True
+
+		return False
 
 
 
 if __name__ == "__main__":
+	from sys import argv
+
+	myargs = getopts(argv)
+
 	macro = Macro()
 	macroCLI = MacroCLI()
-	
-	while True:
 		
 
 
