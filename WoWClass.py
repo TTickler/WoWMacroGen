37379479b3378818a4b5be380c9fd6fc0285533d
clannadqs <cdozier3@students.kennesaw.edu>
2018-03-20 22:32:27 -0400

    WoWClass module sees Class inherit race instead of Race inheriting Class. Properties in Race and Class added

--- WoWClass.py
+++ WoWClass.py
@@ -1,26 +1,44 @@
 import os
 import ConfigParser
 
+class Race(object):
+	def __init__(self, race):
+		self.race = race
 
-class Class(object):
-	def __init__(self):
 		self.Config = ConfigParser()
-		self.Config.read(os.getcwd() + "")
+		self.Config.read(os.getcwd() + '/../Classes/Racials.ini')
 
+	@property
+	def race(self):
+		return self._race
 
-
-class Race(Class):
-	def __init__(self):
-		self.test = 5
+	@race.setter
+	def race(self, race):
+		self._race = race
 
 
 	@property
 	def racial(self):
-		return self._racial
+		racial = self.Config.get(self._race , 'racial')
+		return racial
 
-	@racial.setter
-	def racial(self, racial):
-		self._racial = 
+class Class(Race):
+	def __init__(self, wClass, race):
+		Race.__init__(self, race)
+
+		self.Config = ConfigParser()
+		self.Config.read(os.getcwd() + '/../Classes/' + wClass + '.ini')
+
+		self.type = wClass
+
+		@property
+		def type(self):
+			return self._type
+
+		@type.setter
+		def type(self, type):
+			self._type = type
+
 
 class Spec(Class):
 	def __init__(self):
